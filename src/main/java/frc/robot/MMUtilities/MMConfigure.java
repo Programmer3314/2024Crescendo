d50883f0ccfafae8bfa84d9c529b7be52c873543
Lamarr3314 <lamarrellis6@gmail.com>
2024-01-19 23:08:38 -0500

    formatted

---
+++ src/main/java/frc/robot/MMUtilities/MMConfigure.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.MMUtilities;
+
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+
+/** Add your docs here. */
+public class MMConfigure {
+    // TODO: Add similar method for configuring CANCoders
+    // TODO: Let's discuss naming of configureMotor (more or less specific?)
+    public static void configureDevice(TalonFX motor, TalonFXConfiguration cfg) {
+        StatusCode status = StatusCode.StatusCodeNotInitialized;
+        for (int i = 0; i < 5; ++i) {
+            status = motor.getConfigurator().apply(cfg);
+            if (status.isOK()) {
+                break;
+            }
+        }
+        if (!status.isOK()) {
+            System.out.println("Could not apply configs, error code: " + status.toString());
+        }
+    }
+
+    public static void configureDevice(CANcoder canCoder, CANcoderConfiguration cfg) {
+        StatusCode status = StatusCode.StatusCodeNotInitialized;
+        for (int i = 0; i < 5; ++i) {
+            status = canCoder.getConfigurator().apply(cfg);
+            if (status.isOK()) {
+                break;
+            }
+        }
+        if (!status.isOK()) {
+            System.out.println("Could not apply configs, error code: " + status.toString());
+        }
+    }
+
+}
