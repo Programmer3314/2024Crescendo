ca0ddb72b160f7c97589bb304db840a8b51548b8
Rich Kopelow <rich.kopelow@gmail.com>
2024-02-18 00:45:56 -0500

    cleanup

--- src/main/java/frc/robot/MMUtilities/MMFiringSolution.java
+++ src/main/java/frc/robot/MMUtilities/MMFiringSolution.java
@@ -19,34 +19,37 @@ public class MMFiringSolution {
 
     // make Waypoint and MMFiringSolution use
     // Angle, Left & Right velocities, and Index Speed.
+
+    // TODO: review the use of the res variable. 
     public MMWaypoint calcSolution(double distance) {
         MMWaypoint res = null;
         MMWaypoint bottomRef = null;
         MMWaypoint topRef = null;
+
         if (distance <= waypoints[0].getDistance()) {
             res = waypoints[0];
-        }
-        if (distance >= waypoints[waypoints.length - 1].getDistance()) {
+        } else if (distance >= waypoints[waypoints.length - 1].getDistance()) {
             res = waypoints[waypoints.length - 1];
-        }
-        for (int i = 1; i < waypoints.length; i++) {// find the reference waypoints
-            if (distance < waypoints[i].getDistance()) {
-                topRef = waypoints[i];
-                bottomRef = waypoints[i - 1];
-                break;
+        } else {
+            for (int i = 1; i < waypoints.length; i++) {// find the reference waypoints
+                if (distance < waypoints[i].getDistance()) {
+                    topRef = waypoints[i];
+                    bottomRef = waypoints[i - 1];
+                    break;
+                }
             }
+            double refDistance = topRef.getDistance() - bottomRef.getDistance();
+            double scale = (distance - bottomRef.getDistance()) / refDistance;
+            double desiredAngle = (topRef.getAngle() - bottomRef.getAngle()) * scale + bottomRef.getAngle();
+            double desiredVelocity = (topRef.getVelocity() - bottomRef.getVelocity()) * scale + bottomRef.getVelocity();
+
+            double desiredLeftVelocity = (topRef.getLeftVelocity() - bottomRef.getLeftVelocity()) * scale
+                    + bottomRef.getLeftVelocity();
+            double desiredRightVelocity = (topRef.getRightVelocity() - bottomRef.getRightVelocity()) * scale
+                    + bottomRef.getRightVelocity();
+
+            res = new MMWaypoint(distance, desiredAngle, desiredLeftVelocity, desiredRightVelocity, desiredVelocity);
         }
-        double refDistance = topRef.getDistance() - bottomRef.getDistance();
-        double scale = (distance - bottomRef.getDistance()) / refDistance;
-        double desiredAngle = (topRef.getAngle() - bottomRef.getAngle()) * scale + bottomRef.getAngle();
-        double desiredVelocity = (topRef.getVelocity() - bottomRef.getVelocity()) * scale + bottomRef.getVelocity();
-
-        double desiredLeftVelocity = (topRef.getLeftVelocity() - bottomRef.getLeftVelocity()) * scale
-                + bottomRef.getLeftVelocity();
-        double desiredRightVelocity = (topRef.getRightVelocity() - bottomRef.getRightVelocity()) * scale
-                + bottomRef.getRightVelocity();
-
-        res = new MMWaypoint(distance, desiredAngle, desiredLeftVelocity, desiredRightVelocity, desiredVelocity);
 
         // Alternate angle calc...
         // 34 degrees and .389 encoder value
@@ -58,22 +61,20 @@ public class MMFiringSolution {
         double shootAngle = Math.atan2((speakerHeight - pivotHeight), distance) / (2 * Math.PI) + .294;
         if (shootAngle > .458) {
             shootAngle = .458;
-
         }
         if (shootAngle < .385) {
             shootAngle = .385;
         }
-
         SmartDashboard.putNumber("fsRise", (speakerHeight - pivotHeight));
         SmartDashboard.putNumber("fsRun", distance);
         SmartDashboard.putNumber("fsAtan2", Math.atan2((speakerHeight - pivotHeight), distance));
         SmartDashboard.putNumber("fsDistance", distance);
         SmartDashboard.putNumber("fsShootAngle", shootAngle);
-        SmartDashboard.putNumber("fsDesiredAngle", desiredAngle);
+        SmartDashboard.putNumber("fsDesiredAngle", res.getAngle());
 
-        return new MMWaypoint(res.getDistance(), shootAngle, res.getLeftVelocity(), res.getRightVelocity(), res.getVelocity());
+        return new MMWaypoint(distance, shootAngle, res.getLeftVelocity(), res.getRightVelocity(),
+                res.getVelocity());
         // return new MMWaypoint(distance, desiredAngle, desiredLeftVelocity,
         // desiredRightVelocity, desiredVelocity);
     }
-
 }
