c5f52366029003007b44044783a0447f53ca64a2
programmer3314 <programmer3314@gmail.com>
2024-10-10 20:30:22 -0400

    l3Now

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -46,7 +46,7 @@ public class TunerConstants {
     // This may need to be tuned to your individual robot
     private static final double kCoupleRatio = 3.5714285714285716;
 
-    private static final double kDriveGearRatio = 6.746031746031747; // L3 =6.122448979591837
+    private static final double kDriveGearRatio = 6.122448979591837;//L2=6.746031746031747 // L3 =6.122448979591837
     private static final double kSteerGearRatio = 12.8;
     private static final double kWheelRadiusInches = 2;
 
